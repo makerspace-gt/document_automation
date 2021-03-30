5574efd96900971eca951de7fe6a8a3ea61e18af
Martin Teuber <martinteuber@web.de>
2021-03-30 22:40:02 +0200

    Add TransferRigthsOfUse contract for movieproject

---
+++ Vertraege/Urheberrechtsuebertragung/21-03-30_Filmprojekt_Wissenshaeppchen_3D-Druck/Build_Windows.bat
@@ -0,0 +1,16 @@
+@echo off
+rem Builds for Transfer_Rights_of_Use_Wissenshaeppchen_3D-Druck.rmd
+rem @author: Martin Teuber
+
+call ..\..\..\Systemvorraussetzungen\Windows\scripts\findR.bat
+if errorlevel 1 goto norexe
+
+rem Install requirements
+"%rexe%" "build.r"
+goto :eof
+
+:norexe
+echo: & echo.^
+No R.exe found. Maybe you need to install the Windows systems & echo.^
+Requirements under document-automation/Systemvorraussetzungen/Windows
+pause
